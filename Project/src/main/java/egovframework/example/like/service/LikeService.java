32eaa06a45e682def962509806074fa84f1b8f23
kangseungtae4704 <jintmdxo@naver.com>
2025-07-11 14:09:24 +0900

    2025년 07월 11일 오후 2시 10분
    2025년 07월 11일 오후 2시 10분
    
    좋아요 DB에 보이도록 수정

--- Project/src/main/java/egovframework/example/like/service/LikeService.java
+++ Project/src/main/java/egovframework/example/like/service/LikeService.java
@@ -14,7 +14,8 @@ public interface LikeService {
     void addLike(LikeVO likevo);
     void removeLike(LikeVO likevo);
     boolean checkLike(LikeVO likeVO) ;
-
+    void increaseLikeCount(int boardId); // 7월 11일 좋아요 카운트 DB에볼수있게할려고 추가: 강승태
+    void decreaseLikeCount(int boardId); // 7월 11일 좋아요 카운트 DB에볼수있게할려고 추가: 강승태
     // (선택) 리스트가 필요하면:
     List<LikeVO> selectLikeListByBoardId(@Param("boardId") int boardId);
 
